0640063f7c50eff285cddce25159fb588c3fb8b7
anuraaagg <s.anurag1998@gmail.com>
2026-01-13 17:48:59 +0530

    Initial Commit

---
+++ SceneColor/SceneColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceneColorApp.swift
+//  SceneColor
+//
+//  Created by Anurag Singh on 13/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SceneColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
